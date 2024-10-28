697f86e3a5f31d951061a0238dc705ae2aad9cdb
Shook <shooklyngs@gmail.com>
2024-10-28 11:43:03 +0800

    ref: update "api/static-min-fees" API to reduce size

--- api/static-min-fees.ts
+++ api/static-min-fees.ts
@@ -4,12 +4,9 @@ export default function handler(_req: VercelRequest, res: VercelResponse) {
   return res.json({
     "current_block_hash": "000000000000000000006bc6b05711f10caa17a20be49f0a06067af582722543",
     "fee_by_block_target": {
-      "2": 1012,
       "3": 1012,
-      "4": 1012,
-      "5": 1012,
       "6": 1012,
-      "25": 1012
+      "10": 1012
     },
     "min_relay_feerate": 1012
   });
