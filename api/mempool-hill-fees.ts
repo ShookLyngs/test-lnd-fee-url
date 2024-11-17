7fae7c0b3b70599a73630a69ac97e9601cb2559d
Shook <shooklyngs@gmail.com>
2024-11-18 06:04:02 +0800

    feat: add simple error handling for mempool apis

--- api/mempool-hill-fees.ts
+++ api/mempool-hill-fees.ts
@@ -2,23 +2,33 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { toMsatPerVb, MempoolService } from '../lib/mempool.js';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-  const mempool = MempoolService.fromRequest(req);
-  const [fees, tipHash] = await Promise.all([
-    mempool.getFeesRecommended(),
-    mempool.getBlocksTipHash(),
-  ]);
+  try {
+    const mempool = MempoolService.fromRequest(req);
+    const [fees, tipHash] = await Promise.all([
+      mempool.getFeesRecommended(),
+      mempool.getBlocksTipHash(),
+    ]);
 
-  console.log(`tip-hash: ${tipHash}, fees: ${JSON.stringify(fees)}`);
+    console.log(`tip-hash: ${tipHash}, fees: ${JSON.stringify(fees)}`);
 
-  return res.json({
-    "current_block_hash": tipHash,
-    "fee_by_block_target": {
-      "3": 1012,
-      "4": toMsatPerVb(fees.fastestFee),
-      "6": toMsatPerVb(fees.halfHourFee),
-      "8": toMsatPerVb(fees.hourFee),
-      "10": toMsatPerVb(fees.economyFee),
-    },
-    "min_relay_feerate": 1012
-  });
+    return res.json({
+      "current_block_hash": tipHash,
+      "fee_by_block_target": {
+        "3": 1012,
+        "4": toMsatPerVb(fees.fastestFee),
+        "6": toMsatPerVb(fees.halfHourFee),
+        "8": toMsatPerVb(fees.hourFee),
+        "10": toMsatPerVb(fees.economyFee),
+      },
+      "min_relay_feerate": 1012
+    });
+  } catch (e: any) {
+    res.status(500).send({
+      error: {
+        code: e?.code ?? e.status ?? null,
+        message: e?.message ?? 'internal server error',
+      },
+    });
+    throw e;
+  }
 }
--- api/mempool-min-fees.ts
+++ api/mempool-min-fees.ts
@@ -2,20 +2,30 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { toMsatPerVb, MempoolService } from '../lib/mempool.js';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-  const mempool = MempoolService.fromRequest(req);
-  const [fees, tipHash] = await Promise.all([
-    mempool.getFeesRecommended(),
-    mempool.getBlocksTipHash(),
-  ]);
+  try {
+    const mempool = MempoolService.fromRequest(req);
+    const [fees, tipHash] = await Promise.all([
+      mempool.getFeesRecommended(),
+      mempool.getBlocksTipHash(),
+    ]);
 
-  console.log(`tip-hash: ${tipHash}, fees: ${JSON.stringify(fees)}`);
+    console.log(`tip-hash: ${tipHash}, fees: ${JSON.stringify(fees)}`);
 
-  const minimumFee = toMsatPerVb(fees.minimumFee);
-  return res.json({
-    "current_block_hash": tipHash,
-    "fee_by_block_target": {
-      "3": minimumFee,
-    },
-    "min_relay_feerate": minimumFee,
-  });
+    const minimumFee = toMsatPerVb(fees.minimumFee);
+    return res.json({
+      "current_block_hash": tipHash,
+      "fee_by_block_target": {
+        "3": minimumFee,
+      },
+      "min_relay_feerate": minimumFee,
+    });
+  } catch (e: any) {
+    res.status(500).send({
+      error: {
+        code: e?.code ?? e.status ?? null,
+        message: e?.message ?? 'internal server error',
+      },
+    });
+    throw e;
+  }
 }
