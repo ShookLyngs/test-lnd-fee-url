56d5a3459143df2a1a5e8fd3df2a039a06531f91
Shook <shooklyngs@gmail.com>
2024-11-18 05:51:38 +0800

    feat: add a fallback calculation for the mempool recommended-fees api

--- api/mempool-hill-fees.ts
+++ api/mempool-hill-fees.ts
@@ -1,24 +1,23 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { fromMempoolFee, fromRecommendedFees, setupMempool } from './_mempool.js';
+import { toMsatPerVb, MempoolService } from '../lib/mempool.js';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-  const { mempool } = setupMempool(req);
-  const [mempoolFees, tipHash] = await Promise.all([
-    mempool.bitcoin.fees.getFeesRecommended(),
-    mempool.bitcoin.blocks.getBlocksTipHash(),
+  const mempool = MempoolService.fromRequest(req);
+  const [fees, tipHash] = await Promise.all([
+    mempool.getFeesRecommended(),
+    mempool.getBlocksTipHash(),
   ]);
 
-  const fees = fromRecommendedFees(mempoolFees);
   console.log(`tip-hash: ${tipHash}, fees: ${JSON.stringify(fees)}`);
 
   return res.json({
     "current_block_hash": tipHash,
     "fee_by_block_target": {
       "3": 1012,
-      "4": fromMempoolFee(fees.fastestFee),
-      "6": fromMempoolFee(fees.halfHourFee),
-      "8": fromMempoolFee(fees.hourFee),
-      "10": fromMempoolFee(fees.economyFee),
+      "4": toMsatPerVb(fees.fastestFee),
+      "6": toMsatPerVb(fees.halfHourFee),
+      "8": toMsatPerVb(fees.hourFee),
+      "10": toMsatPerVb(fees.economyFee),
     },
     "min_relay_feerate": 1012
   });
--- api/mempool-min-fees.ts
+++ api/mempool-min-fees.ts
@@ -1,16 +1,16 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { fromMempoolFee, setupMempool } from './_mempool.js';
+import { toMsatPerVb, MempoolService } from '../lib/mempool.js';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-  const { mempool } = setupMempool(req);
+  const mempool = MempoolService.fromRequest(req);
   const [fees, tipHash] = await Promise.all([
-    mempool.bitcoin.fees.getFeesRecommended(),
-    mempool.bitcoin.blocks.getBlocksTipHash(),
+    mempool.getFeesRecommended(),
+    mempool.getBlocksTipHash(),
   ]);
 
   console.log(`tip-hash: ${tipHash}, fees: ${JSON.stringify(fees)}`);
 
-  const minimumFee = fromMempoolFee(fees.minimumFee);
+  const minimumFee = toMsatPerVb(fees.minimumFee);
   return res.json({
     "current_block_hash": tipHash,
     "fee_by_block_target": {
